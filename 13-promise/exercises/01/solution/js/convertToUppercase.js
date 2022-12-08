e53bac3d876ea67238b148c73d8de3b51c669816
timivarga <timea.varga252@gmail.com>
2022-12-08 11:38:48 +0100

    kesz

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,25 @@
+const convertToUppercase = (array) => {
+  return new Promise((resolve, reject) => {
+    let result = [];
+    if (array.every((element) => typeof element === "string")) {
+      result = array.map((element) => element.toUpperCase());
+      resolve(result);
+    } else {
+      reject(new Error("Error: Not all items in the array are strings!"));
+    }
+  });
+};
 export default convertToUppercase;
+
+// promise nélkül
+const convertToUppercase2 = (array) => {
+  let result = [];
+  if (array.every((element) => typeof element === "string")) {
+    result = array.map((element) => element.toUpperCase());
+  } else {
+    console.log("Error: Not all items in the array are strings!");
+  }
+  return result;
+};
+
+convertToUppercase2(["first", "second", "third", 11]);
